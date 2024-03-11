6706d18b56ef4c24381190f2bbf2b836d1a0e9f8
Aditya Singh Yadav <97344779+aditya-mnnit@users.noreply.github.com>
2024-03-11 23:33:28 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of Provinces/number-of-provinces.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+  vector<int>vis;
+  void dfs(int node,vector<vector<int>>&adj)
+  {
+      vis[node]=1;
+      for(int i=0;i<adj[node].size();i++)
+      {
+          if(i!=node&&vis[i]==0&&adj[node][i]==1)
+          {
+             dfs(i,adj); 
+          }
+      }
+  }
+    int numProvinces(vector<vector<int>> adj, int V) {
+        // code here
+        vis.assign(V,0);
+        int c=0;
+        for(int i=0;i<V;i++)
+        {
+            if(vis[i]==0)
+            {
+                c++;
+                dfs(i,adj);
+            }
+        }
+        return c;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V,x;
+        cin>>V;
+        
+        vector<vector<int>> adj;
+        
+        for(int i=0; i<V; i++)
+        {
+            vector<int> temp;
+            for(int j=0; j<V; j++)
+            {
+                cin>>x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+        
+
+        Solution ob;
+        cout << ob.numProvinces(adj,V) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
